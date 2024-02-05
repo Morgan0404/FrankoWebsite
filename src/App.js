4fef9ca63bdc32ced1cd5b48871ae813341980a0
Morgan James <mj522@exeter.ac.uk>
2024-02-05 13:14:25 +0000

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+// Import necessary modules and styles
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const user = {
+  name: 'Franco and The Costellos',
+  imageUrl: 'franko.png',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  const handleFollowClick = () => {
+    // Replace 'YOUR_SOUNDCLOUD_PROFILE_URL' with the actual URL of your SoundCloud profile
+    const soundcloudProfileUrl = 'https://soundcloud.com/franco-and-the-costellos';
+
+    // Open the SoundCloud profile in a new tab/window
+    window.open(soundcloudProfileUrl, '_blank');
+  };
+
+  const handleFollowClick2 = () => {
+    const twitterProfileUrl = 'https://twitter.com/AndtheCostellos'
+    window.open(twitterProfileUrl, '_blank');
+  }
+
+
+    return (
+      <div className="container">
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize,
+          }}
+        />
+        
+        <h1>{user.name}</h1>
+        <button className="button" onClick={handleFollowClick}>
+          Follow on SoundCloud
+        </button>
+        < button className = "button" onClick={handleFollowClick2}>
+          Follow on Twitter
+        </button>
+      </div>
+    );
+  }
+
